d2f6cae9df87a99d02d5fe624c804ede8a60e785
J. Degand <70610011+jdegand@users.noreply.github.com>
2024-10-06 18:19:53 +0000

    fix: update environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  API_BASE_URL: "https://api.openweathermap.org/data/2.5/forecast", //weather
+  API_KEY: "open_weather_api_key_goes_here"
 };
